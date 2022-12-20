3b07e7357bc2d28551e887c71c03f4b3dd67caa2
Pat Escalona <pat.escalona7@gmail.com>
2022-12-19 21:10:32 -0600

    React app working Free Chlorine section in progress

--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 
 class FreeChlorine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '0' };
+
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+        // this.specLow = this.specLow.bind(this);
+        // this.specHigh = this.specHigh.bind(this);
+    }
+
     render() {
         return (
-            <div className="free-clorine">
-                <label></label>
-                <input></input>
-                Add 0 of 6 weight% bleach. Jug size 96 oz. or add
-                0 by weight or 0 by volume of trichlor.
-                Note: Dichlor and trichlor add CYA and lower pH. Cal-hypo adds CH.
-            </div>
-        );
+            <div className="free-clorine"> Free Chlorine
+                < div >
+                    <input type="text" name="current" placeholder="current" />
+                    {/* <input type="text" name = "current" value={this.state.value} onChange={this.handleChange} /> */}
+                    <input type="text" name="goal" placeholder="goal"></input>
+                    <div />Add 0 of 6 weight % bleach. </div >
+                <div>Jug size 96 oz. or add 0 by weight or 0 by volume of trichlor.</div>
+                <div>Note: Dichlor and trichlor add CYA and lower pH.Cal - hypo adds CH.
+                </div >
+            </div>);
     }
 }
 
