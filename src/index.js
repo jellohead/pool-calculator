3a576be6ec792a1f580baa603dadd476a093086b
pat <pat.escalona7@gmail.com>
2023-01-10 08:23:45 -0600

    Chlorine section working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,88 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
+import '../src/index.css';
 
+
+function FreeChlorine(props) {
+    const { poolVolume } = props;
+
+    // const [specLow, setSpecLow] = useState(2);
+    // const [specHigh, setSpecHigh] = useState(4);
+    const [bleachPercent, setBleachPercent] = useState(1.25);
+    const [currentPPM, setCurrentPPM] = useState(2);
+    const [targetPPM, setTargetPPM] = useState(4);
+    const [bleachJugSize, setBleachJugSize] = useState(128);
+
+
+    function handleChangeCurrentPPM(event) {
+        setCurrentPPM(event.target.value);
+    }
+
+    function handleChangeTargetPPM(event) {
+        setTargetPPM(event.target.value);
+    }
+
+    function handleChangeBleachPercent(event) {
+        setBleachPercent(event.target.value);
+    }
+
+    function handleChangeBleachJugSize(event) {
+        setBleachJugSize(event.target.value);
+    }
+
+    function bleachVolume() {
+        console.log(bleachPercent);
+        const bleachPPM = bleachPercent * 1000000 / 100;
+        console.log(bleachPPM);
+        console.log(targetPPM, currentPPM);
+        const bleachAmount = (targetPPM - currentPPM) * poolVolume / bleachPPM * 128;
+        console.log(bleachAmount);
+        return bleachAmount;
+    }
+
+    function numberOfJugs(bleachVolume, bleachJugSize) {
+        return Math.ceil(bleachVolume() / bleachJugSize);
+    }
+
+    return (
+        <div className="analysis"> Free Chlorine
+            < div >
+                <label>Current PPM</label>
+                <input
+                    type="text"
+                    name="currentPPM"
+                    value={currentPPM}
+                    onChange={handleChangeCurrentPPM}
+                />
+                <label>Desired PPM</label>
+                <input type="text" name="targetPPM" value={targetPPM} onChange={handleChangeTargetPPM} />
+                <div>
+                    <label>Bleach Concentration (%)</label>
+                    <input
+                        type="text"
+                        name="bleachPercent"
+                        value={bleachPercent}
+                        onChange={handleChangeBleachPercent}
+                    />
+
+                </div>
+                <div>
+                    <label>Bleach Jug Size (ounces)</label>
+                    <input
+                        type="text"
+                        name="bleachJugSize"
+                        value={bleachJugSize}
+                        onChange={handleChangeBleachJugSize}
+                    />
+
+                </div>
+                <div> Add {bleachVolume()} ounces of {bleachPercent}% bleach.</div>
+            </div >
+            <div> You will need {Math.ceil(bleachVolume() / bleachJugSize)} jug{Math.ceil(bleachVolume() / bleachJugSize) === 1 ? '' : 's'} of bleach.</div >
+        </div>
+    )
+
+}
 function Pool() {
     const [poolVolume, setPoolVolume] = useState('10000');
     const [poolMaterial, setPoolMaterial] = useState('');
@@ -35,61 +116,10 @@ function Pool() {
                 <option value="fiberglass">fiberglass</option>
                 <option value="vinyl">vinyl</option>
             </select>
-            < FreeChlorine poolVolume={poolVolume} setPoolVolume={setPoolVolume} />
+            < FreeChlorine poolVolume={poolVolume} />
         </div>
     )
 }
-
-function FreeChlorine(props) {
-    const { poolVolume, setPoolVolume } = props;
-
-    // const [specLow, setSpecLow] = useState(2);
-    // const [specHigh, setSpecHigh] = useState(4);
-    const [bleachPercent, setBleachPercent] = useState(1.25);
-    const [currentPPM, setCurrentPPM] = useState(2);
-    const [targetPPM, setTargetPPM] = useState(4);
-    const [bleachJugSize, setBleachJugSize] = useState(128);
-
-
-    function handleChangeCurrentPPM(event) {
-        setCurrentPPM(event.target.value);
-    }
-
-    function handleChangeTargetPPM(event) {
-        setTargetPPM(event.target.value);
-    }
-
-    function bleachVolume() {
-        console.log(bleachPercent);
-        const bleachPPM = bleachPercent * 1000000 / 100;
-        console.log(bleachPPM);
-        console.log(targetPPM, currentPPM);
-        const bleachAmount = (targetPPM - currentPPM) * poolVolume / bleachPPM;
-        console.log(bleachAmount);
-        return bleachAmount;
-        // * need to account for volume unit conversions to get oz to add
-    }
-
-
-    return (
-        <div className="free-chlorine"> Free Chlorine
-            < div >
-                <input
-                    type="text"
-                    name="currentPPM"
-                    value={currentPPM}
-                    onChange={handleChangeCurrentPPM}
-                />
-                <input type="text" name="targetPPM" value={targetPPM} onChange={handleChangeTargetPPM} />
-                <div> Add {bleachVolume()} of {bleachPercent}% bleach.</div>
-            </div >
-            <div>Jug size 96 oz. or add 0 by weight or 0 by volume of trichlor.</div>
-            <div>Note: Dichlor and trichlor add CYA and lower pH.Cal - hypo adds CH.</div >
-        </div>
-    )
-
-}
-
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
